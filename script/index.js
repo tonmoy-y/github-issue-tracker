46f3b46fc7557781f1a37956a3ae84652d0c779b
tonmoy-y <tonmoysarker451@gmail.com>
2026-03-18 14:06:45 +0600

    first commit

---
+++ script/index.js
@@ -0,0 +1,21 @@
+document.getElementById('login-button').addEventListener('click' ,function (event) { 
+    // event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if(username === 'admin' && password === 'admin123') {
+        window.location.replace('dashboard.html');
+    }
+    else {
+       const wrongPassword = document.getElementById('wrong-password');
+       wrongPassword.innerHTML = `
+       <div role="alert" class="alert alert-warning">
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+  </svg>
+  <span>Warning: Invalid username or password!</span>
+</div>
+       `;
+    }
+
+});
